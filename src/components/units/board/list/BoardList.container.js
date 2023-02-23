dc19ac2f8c51b612bb38099b09a81c967f9e481b
rjsej12 <rjsej12@naver.com>
2023-02-23 20:04:58 +0900

    feat: Add fetchBoards API and Connect BoardList component

--- src/components/units/board/list/BoardList.container.js
+++ src/components/units/board/list/BoardList.container.js
@@ -1,5 +1,9 @@
 import BoardListUI from './BoardList.presenter';
+import { useQuery } from '@apollo/client';
+import { FETCH_BOARDS } from './BoardList.queries';
 
 export default function BoardList() {
-	return <BoardListUI />;
+	const { data } = useQuery(FETCH_BOARDS);
+
+	return <BoardListUI data={data} />;
 }
