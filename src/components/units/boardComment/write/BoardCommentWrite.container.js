8f6d9bb24ccfe089532d4de7b47405022aadbd97
rjsej12 <rjsej12@naver.com>
2023-02-25 10:05:52 +0900

    feat: Add createBoardComment API and Connect BoardCommentWrite Component

--- src/components/units/boardComment/write/BoardCommentWrite.container.js
+++ src/components/units/boardComment/write/BoardCommentWrite.container.js
@@ -1,5 +1,56 @@
+import { useMutation } from '@apollo/client';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import BoardCommentWriteUI from './BoardCommentWrite.presenter';
+import { CREATE_BOARD_COMMENT } from './BoardCommentWrite.queries';
 
 export default function BoardCommentWrite() {
-	return <BoardCommentWriteUI />;
+	const router = useRouter();
+	const [createBoardComment] = useMutation(CREATE_BOARD_COMMENT);
+
+	const [writer, setWriter] = useState('');
+	const [password, setPassword] = useState('');
+	const [contents, setContents] = useState('');
+
+	const handleChangeWriter = (e) => {
+		setWriter(e.target.value);
+	};
+
+	const handleChangePassword = (e) => {
+		setPassword(e.target.value);
+	};
+
+	const handleChangeContents = (e) => {
+		setContents(e.target.value);
+	};
+
+	const handleClickWrite = async () => {
+		if (writer && password && contents) {
+			try {
+				await createBoardComment({
+					variables: {
+						createBoardCommentInput: {
+							writer,
+							password,
+							contents,
+							rating: 0,
+						},
+						boardId: router.query.boardId,
+					},
+				});
+			} catch (error) {
+				alert(error.message);
+			}
+		}
+	};
+
+	return (
+		<BoardCommentWriteUI
+			handleChangeWriter={handleChangeWriter}
+			handleChangePassword={handleChangePassword}
+			handleChangeContents={handleChangeContents}
+			handleClickWrite={handleClickWrite}
+			contents={contents}
+		/>
+	);
 }
--- src/components/units/boardComment/write/BoardCommentWrite.presenter.js
+++ src/components/units/boardComment/write/BoardCommentWrite.presenter.js
@@ -1,6 +1,6 @@
 import * as S from './BoardCommentWrite.styles';
 
-export default function BoardCommentWriteUI() {
+export default function BoardCommentWriteUI(props) {
 	return (
 		<S.Wrapper>
 			<S.TitleWrapper>
@@ -8,14 +8,17 @@ export default function BoardCommentWriteUI() {
 				<S.Title>댓글</S.Title>
 			</S.TitleWrapper>
 			<S.InputWrapper>
-				<S.Input type="text" placeholder="작성자" />
-				<S.Input type="password" placeholder="비밀번호" />
+				<S.Input type="text" placeholder="작성자" onChange={props.handleChangeWriter} />
+				<S.Input type="password" placeholder="비밀번호" onChange={props.handleChangePassword} />
 			</S.InputWrapper>
 			<S.ContentsWrapper>
-				<S.Contents placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다." />
+				<S.Contents
+					onChange={props.handleChangeContents}
+					placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
+				/>
 				<S.BottomWrapper>
-					<S.ContentsLength>0/100</S.ContentsLength>
-					<S.Button>등록하기</S.Button>
+					<S.ContentsLength>{props.contents.length}/100</S.ContentsLength>
+					<S.Button onClick={props.handleClickWrite}>등록하기</S.Button>
 				</S.BottomWrapper>
 			</S.ContentsWrapper>
 		</S.Wrapper>
