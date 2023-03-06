84d6ddeceacba3cd28c1fdce42352356be00795c
rjsej12 <rjsej12@naver.com>
2023-03-07 02:50:27 +0900

    fix: Change import path

--- pages/boards/[boardId]/index.tsx
+++ pages/boards/[boardId]/index.tsx
@@ -1,6 +1,6 @@
-import BoardDetail from '@/src/components/units/board/detail/BoardDetail.container';
-import BoardCommentList from '@/src/components/units/boardComment/list/BoardCommentList.container';
-import BoardCommentWrite from '@/src/components/units/boardComment/write/BoardCommentWrite.container';
+import BoardDetail from 'src/components/units/board/detail/BoardDetail.container';
+import BoardCommentList from 'src/components/units/boardComment/list/BoardCommentList.container';
+import BoardCommentWrite from 'src/components/units/boardComment/write/BoardCommentWrite.container';
 
 export default function BoardDetailPage() {
 	return (
