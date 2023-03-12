f9364696d4846326d8b55ed915b807916a673feb
rjsej12 <rjsej12@naver.com>
2023-03-12 23:05:51 +0900

    feat: Add navigation UI

--- src/commons/layout/navigation/LayoutNavigation.presenter.tsx
+++ src/commons/layout/navigation/LayoutNavigation.presenter.tsx
@@ -1,3 +1,20 @@
+import { Fragment } from 'react';
+import { MenuItem, Wrapper } from './LayoutNavigation.styles';
+
+const NAVIGATION_MENUS = [
+	{ name: '자유게시판', page: '/boards' },
+	{ name: '중고마켓', page: '/markets' },
+	{ name: '마이페이지', page: '/mypages' },
+];
+
 export default function LayoutNavigationUI() {
-	return <div>메뉴</div>;
+	return (
+		<Wrapper>
+			{NAVIGATION_MENUS.map((el) => (
+				<Fragment key={el.page}>
+					<MenuItem id={el.page}>{el.name}</MenuItem>
+				</Fragment>
+			))}
+		</Wrapper>
+	);
 }
---
+++ src/commons/layout/navigation/LayoutNavigation.styles.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+	height: 64px;
+	background-color: #ffd600;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 18px;
+	gap: 80px;
+`;
+
+export const MenuItem = styled.div`
+	cursor: pointer;
+
+	:hover {
+		color: #ffffff;
+	}
+`;
