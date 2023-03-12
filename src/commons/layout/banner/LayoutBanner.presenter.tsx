cd9e3784efca9cd7ccfff1bf0d2fbdaef6ef9bde
rjsej12 <rjsej12@naver.com>
2023-03-12 22:46:22 +0900

    feat: Add carousel using react-slick

--- src/commons/layout/banner/LayoutBanner.presenter.tsx
+++ src/commons/layout/banner/LayoutBanner.presenter.tsx
@@ -1,3 +1,28 @@
+import Slider from 'react-slick';
+import { SliderItem, Wrapper } from './LayoutBanner.styles';
+
 export default function LayoutBannerUI() {
-	return <div>배너</div>;
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	};
+
+	return (
+		<Wrapper>
+			<Slider {...settings}>
+				<div>
+					<SliderItem src="/images/banner/carousel1.png" />
+				</div>
+				<div>
+					<SliderItem src="/images/banner/carousel2.png" />
+				</div>
+				<div>
+					<SliderItem src="/images/banner/carousel3.png" />
+				</div>
+			</Slider>
+		</Wrapper>
+	);
 }
