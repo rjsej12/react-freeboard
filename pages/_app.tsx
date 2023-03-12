b19c9541db25ff6eabee6f37ced6f2e899a175e0
rjsej12 <rjsej12@naver.com>
2023-03-12 18:19:06 +0900

    feat: Add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from 'src/commons/layout';
 import ApolloSetting from 'src/components/commons/apollo';
+import { Global } from '@emotion/react';
+import { globalStyles } from 'src/commons/styles/globalStyles';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ApolloSetting>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+			<>
+				<Global styles={globalStyles} />
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</>
 		</ApolloSetting>
 	);
 }
