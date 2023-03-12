315c3c9c2998304f89cf7a7a4b8de7db9c6c74a8
rjsej12 <rjsej12@naver.com>
2023-03-12 18:14:59 +0900

    feat: Divide apollo setting from _app.tsx
    _app.tsx에서 apollo setting을 분리해서관리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,14 @@
 import '../styles/globals.css';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import type { AppProps } from 'next/app';
 import Layout from 'src/commons/layout';
+import ApolloSetting from 'src/components/commons/apollo';
 
 export default function App({ Component, pageProps }: AppProps) {
-	const client = new ApolloClient({
-		uri: 'https://backendonline.codebootcamp.co.kr/graphql',
-		cache: new InMemoryCache(),
-	});
-
 	return (
-		<ApolloProvider client={client}>
+		<ApolloSetting>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
-		</ApolloProvider>
+		</ApolloSetting>
 	);
 }
