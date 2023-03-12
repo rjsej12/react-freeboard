3037096124b5233f25b1cf995257ba08b02618c8
rjsej12 <rjsej12@naver.com>
2023-03-12 18:55:14 +0900

    feat: Add handler function of click header buttons
    로고, 로그인, 회원가입 버튼 클릭할 때 일어나는 핸들러 함수 추가

--- src/commons/layout/header/LayoutHeader.presenter.tsx
+++ src/commons/layout/header/LayoutHeader.presenter.tsx
@@ -1,16 +1,17 @@
 import * as S from './LayoutHeader.styles';
+import type { ILayoutHeaderProps } from './LayoutHeader.types';
 
-export default function LayoutHeaderUI() {
+export default function LayoutHeaderUI(props: ILayoutHeaderProps) {
 	return (
 		<S.Wrapper>
 			<S.InnerWrapper>
-				<S.InnerLogo>
+				<S.InnerLogo onClick={props.handleClickLogo}>
 					<S.AccentColor>{'{ }'} </S.AccentColor>
 					project
 				</S.InnerLogo>
 				<S.ButtonWrapper>
-					<S.LoginButton>로그인</S.LoginButton>
-					<S.SignupButton>회원가입</S.SignupButton>
+					<S.LoginButton onClick={props.handleClickMoveToLogin}>로그인</S.LoginButton>
+					<S.SignupButton onClick={props.handleClickMoveToSignup}>회원가입</S.SignupButton>
 				</S.ButtonWrapper>
 			</S.InnerWrapper>
 		</S.Wrapper>
