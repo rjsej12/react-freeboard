95ca4236a56896d462ef66de46439140e76bb8b7
rjsej12 <rjsej12@naver.com>
2023-03-15 01:42:51 +0900

    fix: Add type of IBoardCommentListUIProps

--- src/components/units/boardComment/list/BoardCommentList.types.ts
+++ src/components/units/boardComment/list/BoardCommentList.types.ts
@@ -2,6 +2,7 @@ import type { IBoardComment, IQuery } from 'src/commons/types/generated/types';
 
 export interface IBoardCommentListUIProps {
 	data?: Pick<IQuery, 'fetchBoardComments'>;
+	handleLoadMore: () => void;
 }
 
 export interface IBoardCommentItemProps {
