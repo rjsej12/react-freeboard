c1f56058a301f95bfe278ce254e1cb72465ea0ee
rjsej12 <rjsej12@naver.com>
2023-03-14 18:46:57 +0900

    feat: Add page style with isActive prop

--- src/components/commons/pagination/Pagination.styles.ts
+++ src/components/commons/pagination/Pagination.styles.ts
@@ -1,11 +1,15 @@
 import styled from '@emotion/styled';
+import type { IPageProps } from './Pagination.types';
 
 export const PageWrapper = styled.div`
 	display: flex;
-	gap: 10px;
+	gap: 12px;
 	align-items: center;
+	font-size: 18px;
 `;
 
 export const Page = styled.span`
-	cursor: pointer;
+	color: ${(props: IPageProps) => (props.isActive ? '#ffd600' : '#000000')};
+	font-weight: ${(props: IPageProps) => (props.isActive ? '700' : '400')};
+	cursor: ${(props: IPageProps) => (props.isActive ? 'none' : 'pointer')};
 `;
