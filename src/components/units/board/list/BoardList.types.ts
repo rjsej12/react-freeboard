6d57106fa2e4b02b2b14df26b01c1e8700f4bf58
rjsej12 <rjsej12@naver.com>
2023-03-14 18:47:26 +0900

    fix: Add type of IBoardListUIProps

--- src/components/units/board/list/BoardList.types.ts
+++ src/components/units/board/list/BoardList.types.ts
@@ -1,8 +1,13 @@
+import type { ApolloQueryResult } from '@apollo/client';
 import type { MouseEvent } from 'react';
-import type { IQuery } from 'src/commons/types/generated/types';
+import type { IQuery, IQueryFetchBoardsArgs } from 'src/commons/types/generated/types';
 
 export interface IBoardListUIProps {
 	data?: Pick<IQuery, 'fetchBoards'>;
 	handleClickMoveToBoardNew: () => void;
 	handleClickMoveToBoardDetail: (e: MouseEvent<HTMLDivElement>) => void;
+	refetch: (
+		variables?: Partial<IQueryFetchBoardsArgs> | undefined
+	) => Promise<ApolloQueryResult<Pick<IQuery, 'fetchBoards'>>>;
+	count?: number;
 }
