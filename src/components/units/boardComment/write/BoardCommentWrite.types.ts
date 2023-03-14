fa0e92a432108ca5874cd8ef67776e34d8f5d21f
rjsej12 <rjsej12@naver.com>
2023-03-15 02:29:20 +0900

    feat: Add IBoardCommentWriteProps, IBoardCommentWriteUIProps and IUpdateBoardCommentInputProps types

--- src/components/units/boardComment/write/BoardCommentWrite.types.ts
+++ src/components/units/boardComment/write/BoardCommentWrite.types.ts
@@ -1,12 +1,28 @@
 import type { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import type { IBoardComment } from 'src/commons/types/generated/types';
+
+export interface IBoardCommentWriteProps {
+	isEdit?: boolean;
+	setIsEdit?: Dispatch<SetStateAction<boolean>>;
+	el?: IBoardComment;
+}
 
 export interface IBoardCommentWriteUIProps {
 	handleChangeWriter: (e: ChangeEvent<HTMLInputElement>) => void;
 	handleChangePassword: (e: ChangeEvent<HTMLInputElement>) => void;
 	handleChangeContents: (e: ChangeEvent<HTMLTextAreaElement>) => void;
 	handleClickWrite: () => void;
+	handleClickUpdate: () => void;
 	writer: string;
 	password: string;
 	contents: string;
+	star: number;
 	setStar: Dispatch<SetStateAction<number>>;
+	isEdit?: boolean;
+	el?: IBoardComment;
+}
+
+export interface IUpdateBoardCommentInputProps {
+	contents?: string;
+	rating?: number;
 }
