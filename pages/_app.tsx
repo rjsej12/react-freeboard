b05372318af7e13f93852759aade1f304fa53880
rjsej12 <rjsej12@naver.com>
2023-03-14 17:47:39 +0900

    feat: Change import path of Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app';
-import Layout from 'src/commons/layout';
 import ApolloSetting from 'src/components/commons/apollo';
 import { Global } from '@emotion/react';
 import { globalStyles } from 'src/commons/styles/globalStyles';
+import Layout from 'src/components/commons/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
