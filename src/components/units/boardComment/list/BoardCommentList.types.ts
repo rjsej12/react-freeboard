8d6ba7d9926633945a4ee6bc40b819d59f274c31
rjsej12 <rjsej12@naver.com>
2023-03-15 01:22:53 +0900

    feat: Add IBoardCommentItemProps type

--- src/components/units/boardComment/list/BoardCommentList.types.ts
+++ src/components/units/boardComment/list/BoardCommentList.types.ts
@@ -1,7 +1,9 @@
-import type { MouseEvent } from 'react';
-import type { IQuery } from 'src/commons/types/generated/types';
+import type { IBoardComment, IQuery } from 'src/commons/types/generated/types';
 
 export interface IBoardCommentListUIProps {
 	data?: Pick<IQuery, 'fetchBoardComments'>;
-	handleClickDelete: (e: MouseEvent<HTMLImageElement>) => void;
+}
+
+export interface IBoardCommentItemProps {
+	el: IBoardComment;
 }
