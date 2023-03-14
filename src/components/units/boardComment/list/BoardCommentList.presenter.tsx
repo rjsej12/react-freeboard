734a6fa33a2c864d546c35617fb043f3df8bd15b
rjsej12 <rjsej12@naver.com>
2023-03-15 01:42:11 +0900

    feat: Add infinite scroll to BoardCommentList
    댓글에 무한스크롤 추가

--- src/components/units/boardComment/list/BoardCommentList.presenter.tsx
+++ src/components/units/boardComment/list/BoardCommentList.presenter.tsx
@@ -1,12 +1,13 @@
+import InfiniteScroll from 'react-infinite-scroller';
 import BoardCommentItem from './BoardCommentItem';
 import type { IBoardCommentListUIProps } from './BoardCommentList.types';
 
 export default function BoardCommentListUI(props: IBoardCommentListUIProps) {
 	return (
-		<>
+		<InfiniteScroll pageStart={0} loadMore={props.handleLoadMore} hasMore={true}>
 			{props.data?.fetchBoardComments.map((el) => (
 				<BoardCommentItem key={el._id} el={el} />
 			))}
-		</>
+		</InfiniteScroll>
 	);
 }
