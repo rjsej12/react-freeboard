f284612740923f86f279828b6694d21d3bf05882
rjsej12 <rjsej12@naver.com>
2023-03-21 20:37:17 +0900

    feat: Connect Searchbar component to BoardList component

--- src/components/units/board/list/BoardList.container.tsx
+++ src/components/units/board/list/BoardList.container.tsx
@@ -4,13 +4,17 @@ import { FETCH_BOARDS, FETCH_BOARDS_COUNT } from './BoardList.queries';
 import { useRouter } from 'next/router';
 import type { IQuery, IQueryFetchBoardsArgs, IQueryFetchBoardsCountArgs } from 'src/commons/types/generated/types';
 import type { MouseEvent } from 'react';
+import { useState } from 'react';
 
 export default function BoardList() {
 	const router = useRouter();
+	const [keyword, setKeyword] = useState('');
+
 	const { data, refetch } = useQuery<Pick<IQuery, 'fetchBoards'>, IQueryFetchBoardsArgs>(FETCH_BOARDS);
-	const { data: dataBoardsCount } = useQuery<Pick<IQuery, 'fetchBoardsCount'>, IQueryFetchBoardsCountArgs>(
-		FETCH_BOARDS_COUNT
-	);
+	const { data: dataBoardsCount, refetch: refetchBoardsCount } = useQuery<
+		Pick<IQuery, 'fetchBoardsCount'>,
+		IQueryFetchBoardsCountArgs
+	>(FETCH_BOARDS_COUNT);
 
 	const handleClickMoveToBoardNew = () => {
 		void router.push('/boards/new');
@@ -20,13 +24,20 @@ export default function BoardList() {
 		void router.push(`/boards/${e.currentTarget.id}`);
 	};
 
+	const handleChangeKeyword = (value: string) => {
+		setKeyword(value);
+	};
+
 	return (
 		<BoardListUI
 			data={data}
 			handleClickMoveToBoardNew={handleClickMoveToBoardNew}
 			handleClickMoveToBoardDetail={handleClickMoveToBoardDetail}
 			refetch={refetch}
+			refetchBoardsCount={refetchBoardsCount}
 			count={dataBoardsCount?.fetchBoardsCount}
+			keyword={keyword}
+			handleChangeKeyword={handleChangeKeyword}
 		/>
 	);
 }
