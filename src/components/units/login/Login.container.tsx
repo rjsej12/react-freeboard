539f6c4e8d390fe1e04178f2423a9005c806531a
rjsej12 <rjsej12@naver.com>
2023-03-29 02:35:55 +0900

    feat: Store accessToken in global state and localStoarge

--- src/components/units/login/Login.container.tsx
+++ src/components/units/login/Login.container.tsx
@@ -7,9 +7,12 @@ import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from './Login.queries';
 import type { IMutation, IMutationLoginUserArgs } from 'src/commons/types/generated/types';
 import type { IFormData } from './Login.types';
+import { useRecoilState } from 'recoil';
+import { accessTokenState } from 'src/commons/store';
 
 export default function Login() {
 	const router = useRouter();
+	const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
 	const [loginUser] = useMutation<Pick<IMutation, 'loginUser'>, IMutationLoginUserArgs>(LOGIN_USER);
 
 	const { register, handleSubmit, formState } = useForm<IFormData>({
@@ -20,13 +23,17 @@ export default function Login() {
 	const handleClickSubmit = async (data: IFormData) => {
 		try {
 			const { email, password } = data;
-			const result = await loginUser({
-				variables: {
-					email,
-					password,
-				},
-			});
-			console.log(result);
+			const result = await loginUser({ variables: { email, password } });
+			const accessToken = result.data?.loginUser.accessToken;
+			console.log(accessToken);
+
+			if (!accessToken) {
+				alert('로그인에 실패했습니다. 다시 시도해 주세요.');
+				return;
+			}
+			setAccessToken(accessToken);
+			localStorage.setItem('accessToken', accessToken);
+			void router.push('/boards');
 		} catch (error) {
 			if (error instanceof Error) alert(error.message);
 		}
