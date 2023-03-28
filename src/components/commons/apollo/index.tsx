fc0273c8a5a24572709936d1676294e6e5a4881a
rjsej12 <rjsej12@naver.com>
2023-03-29 02:29:24 +0900

    feat: Add global state and Connect apollo setting

--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -1,18 +1,32 @@
 import { ApolloClient, ApolloLink, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { accessTokenState } from 'src/commons/store';
 
 interface IApolloSettingProps {
 	children: JSX.Element;
 }
 
+const GLOBAL_STATE = new InMemoryCache();
+
 export default function ApolloSetting(props: IApolloSettingProps) {
+	const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+
+	useEffect(() => {
+		const result = localStorage.getItem('accessToken');
+		// console.log(result);
+		if (result) setAccessToken(result);
+	}, []);
+
 	const uploadLink = createUploadLink({
 		uri: 'https://backendonline.codebootcamp.co.kr/graphql',
+		headers: { Authorization: `Bearer ${accessToken}` },
 	});
 
 	const client = new ApolloClient({
 		link: ApolloLink.from([uploadLink as unknown as ApolloLink]),
-		cache: new InMemoryCache(),
+		cache: GLOBAL_STATE,
 	});
 
 	// prettier-ignore
